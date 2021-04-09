e04013e58d0ce4e5ea5ea090f5e726dfadcf72e7
Lehoax <leolair.pro@gmail.com>
2021-04-09 16:58:33 +0200

    maj mise en page

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 
 
 function sendGitData(name, repoName, url) {
- document.querySelectorAll("button[value="+name+"]")[0].innerHTML  ='<a target="blank" href="'+ url +'"><button id="btn_get_repos"   class="repo_class btn btn-primary" value="tepepeka">'+repoName+'</button></a>'
+    let gitUrl = 'https://github.com/'+name
+    document.querySelectorAll("button[value="+name+"]")[0].innerHTML  ='<a target="blank" href="'+ url +'"><button id="btn_get_repos"   class="repo_class btn btn-primary">'+repoName+'</button></a>'
+    document.querySelectorAll("button[value="+name+"1]")[0].innerHTML  ='<a target="blank" href="'+ gitUrl +'"><button id="btn_get_repos"   class="repo_class btn btn-primary">Git hub</button></a>'
+
 }
 
 async function getApi(name) {
     let response = await fetch("https://api.github.com/users/"+name+"/repos")
     let data = await response.json()
     let git = data[data.length-1]
+    console.log(data);
     let repoName = git.name
     let url =  git.html_url
     let nameUser = git.owner.login
-    console.log(git);
     sendGitData(nameUser, repoName, url) 
 }
 
